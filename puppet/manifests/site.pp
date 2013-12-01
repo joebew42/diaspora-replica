6f232e9fd3c50b2863538cee5b3952c864375eb6
JoeBew42 <joebew42@gmail.com>
2013-12-01 18:54:39 +0100

    Adding node in site.pp

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,13 +1,15 @@
-class { 'diaspora':
-  hostname      => 'staging.diaspora.io',
-  environment   => 'production',
-  app_directory => '/home/diaspora',
-  user          => 'diaspora',
-  group         => 'diaspora',
-  db_provider   => 'mysql',
-  db_host       => 'localhost',
-  db_port       => '3306',
-  db_name       => 'diaspora_production',
-  db_username   => 'diaspora',
-  db_password   => 'diaspora'
+node 'staging.diaspora.io' {
+  class { 'diaspora':
+    hostname      => $fqdn,
+    environment   => 'production',
+    app_directory => '/home/diaspora',
+    user          => 'diaspora',
+    group         => 'diaspora',
+    db_provider   => 'mysql',
+    db_host       => 'localhost',
+    db_port       => '3306',
+    db_name       => 'diaspora_production',
+    db_username   => 'diaspora',
+    db_password   => 'diaspora'
+  }
 }
